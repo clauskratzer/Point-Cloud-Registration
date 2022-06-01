403e11ace5a7a8a77d09b30dc77f7cb2d8df0694
evavoggenreiter <104763494+evavoggenreiter@users.noreply.github.com>
2022-06-01 11:26:37 +0200

    Add files via upload

--- experiment/geod_dist_calculation.py
+++ experiment/geod_dist_calculation.py
@@ -13,7 +13,6 @@ from graph import Vertex, Edge, Dijkstra
 def voxels2graph(grid, size):
     #add edges
     voxels=grid.get_voxels()
-    # print(voxels)
     vertices=[ Vertex(tuple(vx.grid_index),list()) for vx in voxels ]
     for v1 in vertices:
         c1=grid.get_voxel_center_coordinate(np.array(v1.vid))
@@ -24,28 +23,34 @@ def voxels2graph(grid, size):
                 v1.edgelist.append(edge)
     return vertices
 
-def averageDistanceMeasure(graph, printdistances=False):
+def trim_dijkstra_output(samplepoints, di_output):
+    ditrimmed={}
+    
+    for vert in di_output.keys():
+        if (vert in samplepoints):
+            ditrimmed[vert]=di_output.get(vert)
+    return ditrimmed
+
+def averageDistanceMeasure(graph,samplepoints, printdistances=False):
     alpha, beta, gamma = 0.4, 0.2, 0.4 #convex parameters
     di = Dijkstra(graph)
     n= len(di.vertices)
     averageDistance={}
     for v in graph: 
-        #distances=di.dijkstra(v, printdistances)
         distances=di.dijkstra(v, False)
-
-        print(distances)
-        print("________________________________________________________________________________________________________________")
-        eccentricity = max(distances.values())
-        centricity = 1/n*(sum(distances.values()))
+        distances_trimmed=trim_dijkstra_output(samplepoints, distances)
+        eccentricity = max(distances_trimmed.values())
+        centricity = 1/n*(sum(distances_trimmed.values()))
         if printdistances: 
             print("Maximum distance:",eccentricity)
             print("Average distance to all other voxels:",centricity, "\n")
 
         avDistToAllOther =[]
-        for vert in distances.keys():
-            if distances.get(vert)==0:
-                continue
-            avDistToAllOther.append(alpha*distances.get(vert) + beta*eccentricity + gamma*centricity)
+        for vert in distances_trimmed.keys():
+            # TODO: should starting point be skipped?
+            # if distances.get(vert)==0:
+            #     continue
+            avDistToAllOther.append(alpha*distances_trimmed.get(vert) + beta*eccentricity + gamma*centricity)
             if printdistances:
                 print("average distance from vertex ", v.vid, " to vertex ", vert, "\n")
                 print(avDistToAllOther[-1], "\n")  
@@ -55,29 +60,38 @@ def averageDistanceMeasure(graph, printdistances=False):
     return averageDistance
 
 
-#Required input of geodesic distance: (lists of n points, list of m neighbouring points)
-#output of geodesic distance: [n, number of neighbour points] object
-#calculate geodesic dist: from all sampeled points to all sampeled points and from all sampeled points to all points: [m, m+n] m…. number of sampeled points, n number of all points.'
+#Required input of geodesic distance: (lists of n points (sparsly sampled point cloud), list of m neighbouring points (whole point cloud))
+#output of geodesic distance: [m, n] matrix
 def calcPointPairDistances(datapoints, neighbourPoints):
     m,n=len(neighbourPoints), len(datapoints)
-    print(m, n)
-   # distances=np.zeros(shape=(m,m+n), dtype=float)
-    distances=np.zeros(shape=(n,m), dtype=float)
+    distances=np.zeros(shape=(m,n), dtype=float)
     pcd1 = o3d.geometry.PointCloud()
-    pcd1.points=o3d.utility.Vector3dVector(datapoints)
+    pcd1.points=o3d.utility.Vector3dVector(neighbourPoints)
     size=1
     voxel_grid1 = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd1,voxel_size=size)
     graph = voxels2graph(voxel_grid1, size)
 
-    averageDistances = averageDistanceMeasure(graph, True)
+    samplepoints_as_vertices= points_to_vertices(datapoints, size)
+    averageDistances = averageDistanceMeasure(graph,samplepoints_as_vertices,False)
     i=0
+    # insert the distances into matrix
     for k,v in averageDistances.items():
         distances[i,:]=v
         i=i+1
 
     return distances
 
-    
+def points_to_vertices(points, size):
+    #create also vertices for the samplepoints
+    pcd = o3d.geometry.PointCloud()
+    pcd.points=o3d.utility.Vector3dVector(points)
+    voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd,voxel_size=size)
+    graph = voxels2graph(voxel_grid, size)
+    di = Dijkstra(graph)
+    points_as_vertices=[]
+    for v in di.vertices:
+        points_as_vertices.append(v.vid)
+    return points_as_vertices
 
 
 
@@ -101,12 +115,11 @@ if __name__ == "__main__":
 
     #---- Test Case 2 for visualization ----#
     points = np.array([[0,0,0],[1,0,0],[2,0,0],[3,0,0],[3,1,0],[3,2,0],[3,3,0],[0,1,0],[0,2,0],[0,3,0],[0,0,1],[1,0,1],[2,0,1],[3,0,1],[3,1,1],[3,2,1],[3,3,1],[0,1,1],[0,2,1],[0,3,1]])
-    samplepoints = np.array([[0,0,0],[1,0,0],[2,0,0],[3,0,0],[3,1,0],[3,2,0],[3,3,0],[0,1,0],[0,2,0],[0,3,0],[0,0,1],[1,0,1],[2,0,1],[3,0,1],[3,1,1],[3,2,1],[3,3,1],[0,1,1],[0,2,1]])
-    
-
-    distances=calcPointPairDistances(points, samplepoints)
+    samplepoints = np.array([[0,0,0],[1,0,0],[2,0,0],[3,1,0],[3,2,0],[3,3,0],[0,1,0],[0,3,0],[0,0,1],[1,0,1],[2,0,1],[3,0,1],[3,1,1],[3,3,1],[0,1,1],[0,2,1]])
+ 
+    distances=calcPointPairDistances(samplepoints, points)
     print(distances)
-    print(distances.shape) #should be
+    print(distances.shape) #should be 20 times 16
 
 
 
