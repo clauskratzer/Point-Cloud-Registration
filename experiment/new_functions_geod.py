7fd83a966839ae54de070168c196c4018b0c151a
evavoggenreiter <104763494+evavoggenreiter@users.noreply.github.com>
2022-06-09 12:45:20 +0200

    Add files via upload

--- experiment/new_functions_geod.py
+++ experiment/new_functions_geod.py
@@ -15,38 +15,28 @@ def calcPointPairDistances(datapoints, neighbourPoints):
         sp_as_vert=point_to_vertex(datapoints[i,:],voxel_grid1, graph, size)
         distances_dict=di.dijkstra(sp_as_vert)
     
-        for j in range(m): 
-            neighborpoint_as_vertex=point_to_vertex(neighbourPoints[j,:],voxel_grid1, graph, size)
-            distances[i,j]=distances_dict[neighborpoint_as_vertex.vid]
-    
-    return distances
+    #Average distance measure
+        alpha, beta, gamma = 0.4, 0.2, 0.4 #convex parameters
 
-def averageDistanceMeasure(graph,samplepoints, printdistances=False):
-    alpha, beta, gamma = 0.4, 0.2, 0.4 #convex parameters
-    di = Dijkstra(graph)
-    n= len(di.vertices)
-    averageDistance={}
-    for v in samplepoints: 
-        distances=di.dijkstra(v, False)
-        eccentricity = max(distances.values())
-        centricity = 1/n*(sum(distances.values()))
-        if printdistances: 
-            print("Maximum distance:",eccentricity)
-            print("Average distance to all other voxels:",centricity, "\n")
-
+        averageDistance={}
+        
+        eccentricity = max(distances_dict.values())
+        centricity = 1/n*(sum(distances_dict.values()))
+        
         avDistToAllOther ={}
-        for vert in distances.keys():
+        for vert in distances_dict.keys():
             # TODO: should starting point be skipped?
             # if distances.get(vert)==0:
             #     continue
-            avDistToAllOther[vert]=alpha*distances.get(vert) + beta*eccentricity + gamma*centricity
-            if printdistances:
-                print("average distance from vertex ", v.vid, " to vertex ", vert, "\n")
-                print(avDistToAllOther[-1], "\n")  
+            avDistToAllOther[vert]=alpha*distances_dict.get(vert) + beta*eccentricity + gamma*centricity
 
-        averageDistance[v.vid] = avDistToAllOther         
+        for j in range(m): 
+            neighborpoint_as_vertex=point_to_vertex(neighbourPoints[j,:],voxel_grid1, graph, size)
+            distances[i,j]=avDistToAllOther[neighborpoint_as_vertex.vid]
+    
+    return distances
 
-    return averageDistance
+
 
 def point_to_vertex(point,voxel_grid, graph, size):
     #create also vertices for the samplepoints
